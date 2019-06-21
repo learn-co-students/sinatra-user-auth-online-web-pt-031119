bc15146013576a22f46387fd722c33a877cd1a94
iamtash <natashachernis@gmail.com>
2019-06-20 21:45:05 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  #validate some of the attributes of our user by writing code that
+  #makes sure no one can sign up without inputting their name, email, and password
 end
